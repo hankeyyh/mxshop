771515166a0537e048383b701602f064cbdc423e
hankeyyh <hankeyyh@outlook.com>
2023-04-02 20:18:57 +0800

    优雅退出

--- mxshop_srv/user-srv/main.go
+++ mxshop_srv/user-srv/main.go
@@ -1,26 +1,33 @@
 package main
 
 import (
+	"context"
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
 	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
 	"github.com/hankeyyh/mxshop_user_srv/handler"
 	"github.com/hankeyyh/mxshop_user_srv/log"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	//"github.com/hankeyyh/mxshop_user_srv/interceptor"
 	"github.com/hankeyyh/mxshop_user_srv/proto"
 	"google.golang.org/grpc"
-	"net"
 )
 
 /*
 todo 监听ip，端口作为启动参数传入
 */
 func main() {
+	// 中间件注册
 	opt := grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
 		grpc_ctxtags.UnaryServerInterceptor(),
 		grpc_zap.UnaryServerInterceptor(log.DefaultLogger()),
 	))
 
+	// 创建服务
 	server := grpc.NewServer(opt)
 	proto.RegisterUserServer(server, &handler.UserService{})
 	listener, err := net.Listen("tcp", ":8083")
@@ -28,7 +35,19 @@ func main() {
 		panic(err)
 	}
 
-	// todo 主进程退出信号监听
+	// 主进程退出信号监听
+	sgCh := make(chan os.Signal)
+	signal.Notify(sgCh, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		<-sgCh
+		server.GracefulStop()
+		log.Info(context.Background(), "Server Closing...")
+		wg.Done()
+	}()
+
+	// todo 服务注册
 
 	// 启动服务
 	err = server.Serve(listener)
@@ -36,6 +55,6 @@ func main() {
 		panic(err)
 	}
 
-	// todo 服务注册
-
+	wg.Wait()
+	log.Info(context.Background(), "Server Closed")
 }
