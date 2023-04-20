dc489cc6ec94b35e87b173a79e7c1ed8f7e86cfc
hankeyyh <hankeyyh@outlook.com>
2023-04-20 23:56:49 +0800

    健康检查+sms短信配置

--- mxshop-api/user-web/config/config.go
+++ mxshop-api/user-web/config/config.go
@@ -14,9 +14,22 @@ type JWTConfig struct {
 	SigningKey string `mapstructure:"key"`
 }
 
+type AliSmsConfig struct {
+	ApiKey     string `mapstructure:"key"`
+	ApiSecrect string `mapstructure:"secrect"`
+}
+
+type RedisConfig struct {
+	Host   string `mapstructure:"host"`
+	Port   int    `mapstructure:"port"`
+	Expire int    `mapstructure:"expire"`
+}
+
 type ServerConfig struct {
 	Name        string        `mapstructure:"name" json:"name"`
 	UserSrvInfo UserSrvConfig `mapstructure:"user_srv" json:"user_srv"`
 	Log         LogConfig     `mapstructure:"log"`
 	JWTInfo     JWTConfig     `mapstructure:"jwt"`
+	AliSmsInfo  AliSmsConfig  `mapstructure:"sms"`
+	RedisInfo   RedisConfig   `mapstructure:"redis"`
 }
--- mxshop-api/user-web/initialize/router.go
+++ mxshop-api/user-web/initialize/router.go
@@ -5,10 +5,18 @@ import (
 	"go.uber.org/zap"
 	middlewares "mxshop-api/user-web/middleware"
 	"mxshop-api/user-web/router"
+	"net/http"
 )
 
 func InitRouter() *gin.Engine {
 	engine := gin.Default()
+	// 健康检查
+	engine.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    http.StatusOK,
+			"success": true,
+		})
+	})
 
 	// 配置跨域
 	engine.Use(middlewares.Cors())
